542ee9db91db322a453c2249a9591c7a47b33e8a
Pillifino <dinodizon073100@gmail.com>
2022-04-05 09:34:18 -0700

    Added random movie generator and also movie title variable that will eventually display all the movie titles from the random list and search for them in the api call

--- script.js
+++ script.js
@@ -13,16 +13,36 @@ movies = [
     {Title: "Crouching Tiger, Hidden Dragon"}
 ]
 
+var movieArray = []
+
+function randomMovie(movies) {
+  // if (doFetch)
+  var selectedIndex = [] // Blank array that fills with the random indexes from movie object
+  for (var i = 0; i < 5; i++){
+      var randomIndex = Math.floor(Math.random() * movies.length) //Random index generator
+      if (!selectedIndex.includes(randomIndex)){
+        movieArray.push(movies[randomIndex])
+        selectedIndex.push(randomIndex); // If The selected index is not a duplicate then we push the random index into selected index
+      }else{
+        i--; // This removes the duplicate so when the for loop runs again it tries to fill it with a non repeat
+      }
+  }
+  console.log(movieArray)
+}
+
 function doFetch() {
-        fetch('http://www.omdbapi.com/?apikey=91827673', {
+        var movieTitle = 'Blade Runner 2049' // Use this to pace the movie title we want in the API URL
+        fetch('http://www.omdbapi.com/?apikey=91827673&t=' + movieTitle, {
             method: "get"
         }).then(function (response) {
-            return response.text();
-        }).then(function (text) {
-            let json = JSON.parse(text);
+            return response.json();
+        }).then(function (json) {
+            // let json = JSON.parse(text);
+            console.log(json)
             let data = { Title: json.Title, Plot: json.Plot }
             movies.push(data);
             console.log(movies);
+            randomMovie(movies)
         });
     }
 
